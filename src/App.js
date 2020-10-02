e6868f822484edcbf992d5d6c76d14a9548c1be1
goodlana <lahee13579@gmail.com>
2020-10-02 21:43:39 +0900

    4-completed-all

--- src/App.js
+++ src/App.js
@@ -1,30 +1,58 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import  Header  from './components/Header';
-import Main from './components/Main';
 import youtube from './api/youtube'
+import { Grid } from '@material-ui/core'
+import VideoDetails from './components/VideoDetails';
+import VideoList from './components/VideoList'
 
 function App() {
-  const handleSubmit = async(searchTerm) => { 
-  
+  const [videos, setVideos] = useState([])
+  const [selectedVideo, setSelectedVideo] = useState(null)
 
+  const handleSubmit = async(searchTerm) => { 
     const response = await youtube.get('search', {
       params: {
         part: 'snippet',
         maxResults: 5,
-        key: '',
+        key: KEY,
         q: searchTerm
       }
     })
-    console.log(response.data)
+    const responsedArr = response.data.items;
+    const firstVideo = responsedArr[0].id.kind !== 'youtube#channel' ? responsedArr[0] : responsedArr[1]
+
+    setVideos(responsedArr)
+    setSelectedVideo(firstVideo)
   }
   
+  const onVideoSelect = ( video ) => {
+    setSelectedVideo(video)
+  }
+
+  useEffect(()=> {
+    handleSubmit('essential')
+  })
+
   return (
     <div className="App">
       <Header handleSubmit={handleSubmit} />
-      <Main />
-      <footer>
-      </footer>
+      <div className="main-component">
+      <Grid justify="center" container spacing={10}>
+        <Grid item xs={12} >
+          <Grid container spacing={10}>
+            <Grid item xs={8}>
+              <VideoDetails video={selectedVideo}/>
+              <div className="details"></div>
+            </Grid>
+            <Grid item xs={4}>
+              {/* video list */}
+              <VideoList onVideoSelect={onVideoSelect} videos={videos} setSelectedVideo={setSelectedVideo}/>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
+    </div>
     </div>
   );
 }
